c36cbe2a31b7716a2d55856af6790ad24cd1d0e8
Tim <ginatulin.t@yandex.ru>
2021-08-07 21:21:17 +0500

    first commit

--- ms_user/src/main/java/ru/ginatulin/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.ginatulin.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.ginatulin.models.entity.UserEntity;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-}
